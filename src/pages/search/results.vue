e6b03aad3e519ae0b28ac9e5be0567d2d7acd386
Nyussay <nyuusss@icloud.com>
2023-05-02 22:54:47 +0300

    added result table

--- src/pages/search/results.vue
+++ src/pages/search/results.vue
@@ -1,5 +1,31 @@
 <template>
-    <p>It's results page</p>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12">
+        <h1 style="text-align:center; font-weight:bold; font-size:32px">Results for Company:company ID</h1>
+      </v-col>
+      <v-col cols="12">
+        <table style="width:100%">
+          <thead>
+            <tr>
+              <th>Mineral</th>
+              <th>Percentage</th>
+              <th>Purity</th>
+              <th>Moisture</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Value</td>
+              <td>Value</td>
+              <td>Value</td>
+              <td>Value</td>
+            </tr>
+          </tbody>
+        </table>
+      </v-col>
+    </v-row>
+    </v-container>
   </template>
   
   <script lang="ts">
