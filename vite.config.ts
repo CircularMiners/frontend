6d4839f55a566982770fc08e6b4b9845e8052f02
Korawit Rupanya <32285708+KorawitRupanya@users.noreply.github.com>
2023-06-12 21:49:11 +0300

    Fix video not showing

--- vite.config.ts
+++ vite.config.ts
@@ -175,4 +175,5 @@ export default defineConfig({
     // TODO: workaround until they support native ESM
     noExternal: ['workbox-window', /vue-i18n/, 'vuetify'],
   },
+  assetsInclude: ['**/*.mp4'],
 })
